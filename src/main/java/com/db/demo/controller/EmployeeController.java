54ead161baca95e8043f3616cfa75523b61de428
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-01 11:37:27 +0530

    spring-boot-demo

--- src/main/java/com/db/demo/controller/EmployeeController.java
+++ src/main/java/com/db/demo/controller/EmployeeController.java
@@ -1,11 +1,36 @@
 package com.db.demo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.db.demo.model.Employee;
 
 @RestController
 public class EmployeeController {
-	
-	
-	
 
+//	View the output here - 
+//	http://localhost:8090/emp/101
+
+	@GetMapping("emp/{eid}")
+	public Employee getEmpById(@PathVariable(name = "eid") Integer id) {
+		// find the employee by the given id - 
+		Employee employee = new Employee(id, "Sonu", 90.50);
+		System.out.println(employee.toString());
+		return employee;
+	}
+
+	@GetMapping("emp")
+	public List<Employee> getAllEmps() {
+		// find the employees - 
+		return null;
+	}
 }
+
+
+
+
+
+
