40bb30eb13d10097a379cfa78af82b96e9bb2eab
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-01 13:58:57 +0530

    spring-boot-demo

--- src/main/java/com/db/demo/repository/EmployeeRepository.java
+++ src/main/java/com/db/demo/repository/EmployeeRepository.java
@@ -7,5 +7,15 @@ import com.db.demo.model.Employee;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	
+////	insert these records manually in the oracle database 
+//
+//	INSERT INTO emps (id, name, salary) VALUES (101, 'Sonu', 90.75);
+//	INSERT INTO emps (id, name, salary) VALUES (102, 'Monu', 95.50);
+//	INSERT INTO emps (id, name, salary) VALUES (103, 'Tonu', 92.25);
+//
+//	COMMIT;
+//
+//	SELECT * FROM emps;
 
 }
