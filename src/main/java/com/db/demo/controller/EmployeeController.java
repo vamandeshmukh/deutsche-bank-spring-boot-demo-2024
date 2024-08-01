81c0dcaa449a8060d3ddf21cdfef766cb024c6e3
Vaman Deshmukh <vamandeshmukh@gmail.com>
2024-08-01 12:02:14 +0530

    spring-boot-demo

--- src/main/java/com/db/demo/controller/EmployeeController.java
+++ src/main/java/com/db/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.db.demo.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.db.demo.service.IEmployeeService;
 @RestController
 public class EmployeeController {
 
+	@Autowired
 	IEmployeeService empService;
 
 //	View the output here - 
